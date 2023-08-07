df10e66c2f786c109b83014e01354de2904b2b38
Omotayo <kehindeomotayo11@gmail.com>
2023-08-07 16:36:08 +0100

    Completd Assignment

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride, newRide) {
   // Write your code here!
-}
+  const distance = 400;
+  const limit = 2500;
 
-function ternaryCheckCity(){
-  // Write your code here!
+  if (ride < distance) {
+    return "This one is on me!";
+  } else if (ride > distance) {
+    return "I will gladly take your thirty bucks.";
+  } else if (newRide > limit) {
+    let res = "No can do.";
+    console.log(res);
+  }
 }
+console.log(scuberGreetingForFeet(199, 2600));
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// function scuberGreetingForFeet() {
+//   // Write your code here!
+//   const distance1 = 2500;
+//   const ride1 = 3000;
+
+//   if (ride1 > distance1) {
+//     const res = ;
+//     return res;
+//   }
+// }
+// console.log(scuberGreetingForFeet());
+
+function ternaryCheckCity(city) {
+  const msg = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return msg;
 }
+console.log(ternaryCheckCity("NYC"));
+
+function ternaryCheckCity(city) {
+  const msg1 = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return msg1;
+}
+console.log(ternaryCheckCity("NY"));
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+}
+switchOnCharmFromTip(tip);
